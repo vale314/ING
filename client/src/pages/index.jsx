18a5ce3de6cd117c17971b95ed4d96ce95f01d32
Vale314 <electrovale314@hotmail.com>
2019-12-04 19:24:52 -0600

    4.5

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -65,7 +65,7 @@ class Index extends React.Component {
                   opacity: '0.5',
                 }}></div>
                 <video autoPlay loop muted style={{width: "inherit"}}>
-                  <source type="video/mp4" src="https://filebin.net/7ophcbtjspzsfflv/compress.mp4?t=t3cajtbj" />
+                  <source type="video/mp4" src="https://filebin.net/7ophcbtjspzsfflv/compress.mp4?t=61gbgeab" />
                 </video>
             </div>
 
